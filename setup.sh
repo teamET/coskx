75315c86efdbd08bd7eda4689bd43041ba2ee992
sakakendo0321 <sakakendo0321@gmail.com>
2017-12-19 08:35:09 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+sudo apt-get install libconfig
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64
+tar.bz2. tar xvf /phantomjs-2.1.1-linux-x86_64.tar.bz2.
+
+#phantom js test
+phantomjs-2.1.1-linux-x86_64/bin/phantomjs hello.js.
+
+#selenium test
+python main.py.
+
+
+# $uname -a.   Linux JET19 4.4.0-43-Microsoft #1-Microsoft Wed Dec 31 14:42:53 PST 2014 x86_64 x86_64 x86_64 GNU/Linux.
+# 
+# $ls hax
+# README.md. ghostdriver.log. hello.js. main.py. phantomjs-2.1.1-linux-x86_64
+
+
