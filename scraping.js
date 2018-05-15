1fb60ed2edcdb1e1b3925022778d20d17990b0c9
sktrombone <x.file.tip@gmail.com>
2018-05-16 00:49:01 +0900

    finish

--- scraping.js
+++ scraping.js
@@ -7,6 +7,11 @@ const rtm=new RTMClient(process.env.SLACK_TOKEN);
 
 rtm.start();
 
+var slack_id;
+var account_data = fs.readFileSync('./account.json');
+var account = JSON.parse(account_data);
+
+
 function slack(data){
 	if(process.env.SLACK_TOKEN === undefined){
 		console.log('slack token is not defined');
@@ -26,8 +31,8 @@ function slack(data){
 
 const submit=(async(file)=>{
 	var text=''
-	var username='s15097'
-	var password='s15097'
+	var username = account[slack_id["id"]];
+	var password = account[slack_id["pass"]];
 	console.log('submit started');
 	const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 	const page = await browser.newPage();
@@ -36,6 +41,7 @@ const submit=(async(file)=>{
 	await page.type('input[name="userPASS"]',password);
 	await page.click('input[type=button]');
 	await page.waitForNavigation({timeout: 60000, waitUntil: "domcontentloaded"});
+	process.on('unhandledRejection', console.dir);
 	const fileInput = await page.$('input[type=file]');
 	await fileInput.uploadFile(file);
 	await page.click('input[id="sendfiles"]');
@@ -58,26 +64,39 @@ rtm.on('hello',(event)=>{
 });
 
 rtm.on('message',(event)=>{
+	slack_id = event.user;
 	if(event.text.split(' ')[0]==='.h'){
-		slack('hello')
+		slack('hello');
 	}else if(event.text.split(' ')[0]==='.x'){
 		slack('x was sent',event.text.split(' ')[1]);
+	}else if(event.text.split(' ')[0]==='.entry'){
+		var id = event.text.split(' ')[1];
+		var pass = event.text.split(' ')[2];
+		account[slack_id] = {id:id,pass:pass};
+		fs.writeFile('account.json',JSON.stringify(account));
+		slack("Your account has been registered.");
 	}
 	//if(event.text.split(' ')[0]==='.u'){ }
 
 	if(event.subtype && event.subtype==='file_share'){
-		file=download(event.file);
-		text=submit(file)
+		console.log(event.file);
+		file=download(event.file.title,event.file.url_private);
+		if(account[slack_id] !== undefined){
+			text=submit(file);
+		}else{
+			slack("Please register your account."); 
+		}
 	}
 });
 
-function download(file){
+function download(name,url){
 	let headers={Authorization: ' Bearer '+process.env.SLACK_TOKEN};
-	fname='./files/'+file.name;
+	let fname='./files/'+name;
 	request({
-		url:file.url_private,
+		url:url,//file.url_private,
 		headers:{'Authorization': 'Bearer '+process.env.SLACK_TOKEN}})
-			.pipe(fs.createWriteStream('./files/'+file.name));
+			.pipe(fs.createWriteStream(fname));
+	console.log(fname)
 	return fname;
 }
 
